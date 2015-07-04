13d16e105b53e9735bbe62f3e5c14ee736aeb7cd
dgellow <samuel.elborai@gmail.com>
2015-07-04 22:49:06 +0200

    tweak some value

--- src/game/scenes.js
+++ src/game/scenes.js
@@ -10,7 +10,7 @@ function enterGame() {
     // Player
     var p = Crafty.e('Player')
             .controls(4)
-            .setMetrics({x: 100, y: -200}, {
+            .setMetrics({x: 300, y: 320}, {
                 bodyType: 'dynamic',
                 density : 10,
                 friction : 30,
@@ -27,13 +27,13 @@ function enterGame() {
 
     // Wave
     Crafty.e('Wave')
-        .setMetrics({x: 0, y: -200, h: 200}, {
+        .setMetrics({x: -220, y: 80, h: 500, w: 200}, {
             bodyType: 'kinematic',
             density : 10,
             friction : 30,
             restitution : 0
         })
-        .setSpeed(0);
+        .setSpeed(1);
 
     // Setup global events
     bindEvents();
